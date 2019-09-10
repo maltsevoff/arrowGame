33b7a5ce3d8e002bcd844eae4d7a15f15a9ba49b
maltsevoff <alexmalczev@gmail.com>
2019-09-10 20:19:41 +0300

    Arrow hits a target

--- test/GameScene.swift
+++ test/GameScene.swift
@@ -20,6 +20,8 @@ class GameScene: SKScene {
 		let sizeMultiplier: CGFloat = 0.3
 		arrow.size = CGSize(width: arrow.size.width * sizeMultiplier, height: arrow.size.height * sizeMultiplier)
 		addChild(arrow)
+		physicsWorld.gravity = .zero
+		physicsWorld.contactDelegate = self
 		
 		run(SKAction.repeatForever(SKAction.sequence([
 			SKAction.run(addTarget),
@@ -29,9 +31,15 @@ class GameScene: SKScene {
 	
 	func addTarget() {
 		let target = SKSpriteNode(imageNamed: "target")
-		
 		let sizeMultiplier: CGFloat = 0.5
 		target.size = CGSize(width: target.size.width * sizeMultiplier, height: target.size.height * sizeMultiplier)
+		target.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "target"), size: target.size)
+		target.physicsBody?.isDynamic = true
+		target.physicsBody?.categoryBitMask = PhysicsSettings.target
+		target.physicsBody?.contactTestBitMask = PhysicsSettings.arrow
+		target.physicsBody?.collisionBitMask = PhysicsSettings.none
+		print("Target size: \(target.size)")
+		
 		let actualY = size.height * CGFloat.random(in: 0.2 ... 0.9)
 		target.position = CGPoint(x: target.size.width / -2, y: actualY)
 		addChild(target)
@@ -49,6 +57,13 @@ class GameScene: SKScene {
 		arrow.position = position
 		let sizeMultiplier: CGFloat = 0.3
 		arrow.size = CGSize(width: arrow.size.width * sizeMultiplier, height: arrow.size.height * sizeMultiplier)
+		arrow.physicsBody = SKPhysicsBody(rectangleOf: arrow.size)
+		print("Arrow size: \(arrow.size)")
+		arrow.physicsBody?.isDynamic = true
+		arrow.physicsBody?.categoryBitMask = PhysicsSettings.arrow
+		arrow.physicsBody?.contactTestBitMask = PhysicsSettings.target
+		arrow.physicsBody?.collisionBitMask = PhysicsSettings.none
+		arrow.physicsBody?.usesPreciseCollisionDetection = true
 		return arrow
 	}
 	
@@ -63,6 +78,32 @@ class GameScene: SKScene {
 		let actionMove = SKAction.move(to: CGPoint(x: movingArrow.position.x, y: size.height + movingArrow.size.height / 2), duration: 1.0)
 		let actionMoveDone = SKAction.removeFromParent()
 		movingArrow.run(SKAction.sequence([actionMove, actionMoveDone]))
+	}
+}
+
+extension GameScene : SKPhysicsContactDelegate {
+	
+	func arrowDidCollideWithTarget(arrow: SKSpriteNode, target: SKSpriteNode) {
+		print("Hit")
+		arrow.removeFromParent()
+		target.removeFromParent()
+	}
+	
+	func didBegin(_ contact: SKPhysicsContact) {
+		var firstBody: SKPhysicsBody
+		var secondBody: SKPhysicsBody
+		if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+			firstBody = contact.bodyA
+			secondBody = contact.bodyB
+		} else {
+			firstBody = contact.bodyB
+			secondBody = contact.bodyA
+		}
+		if (firstBody.categoryBitMask & PhysicsSettings.target != 0) && (secondBody.categoryBitMask & PhysicsSettings.arrow != 0) {
+			if let target = firstBody.node as? SKSpriteNode, let arrow = secondBody.node as? SKSpriteNode {
+				arrowDidCollideWithTarget(arrow: arrow, target: target)
+			}
+		}
 	}
 }
 
---
+++ test/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  test
+//
+//  Created by Alex Maltsev on 9/10/19.
+//  Copyright © 2019 Aleksandr Maltsev. All rights reserved.
+//
+
+import Foundation
+
+struct PhysicsSettings {
+	static let none : UInt32 = 0
+	static let all : UInt32 = UInt32.max
+	static let target : UInt32 = 0b1
+	static let arrow : UInt32 = 0b10
+}
