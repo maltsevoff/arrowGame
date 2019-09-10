ffca097f67168b1cfb42d6fb69111a69179f4e65
maltsevoff <alexmalczev@gmail.com>
2019-09-10 14:26:46 +0300

    arrow moves and shoots

--- test/GameScene.swift
+++ test/GameScene.swift
@@ -18,7 +18,6 @@ class GameScene: SKScene {
 		let sizeMultiplier: CGFloat = 0.3
 		arrow.size = CGSize(width: arrow.size.width * sizeMultiplier, height: arrow.size.height * sizeMultiplier)
 		addChild(arrow)
-		print(size.width, size.height)
 		
 		run(SKAction.repeatForever(SKAction.sequence([
 			SKAction.run(addTarget),
@@ -43,23 +42,6 @@ class GameScene: SKScene {
 		target.run(SKAction.sequence([actionMove, actionMoveDone]))
 	}
 	
-	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-		guard let touch = touches.first else {
-			return
-		}
-		
-		let frontTouchedNode = atPoint(touch.location(in: self))
-		if frontTouchedNode != arrow {
-			return
-		}
-		
-		let movingArrow = makeArrow(position: arrow.position)
-		addChild(movingArrow)
-		let actionMove = SKAction.move(to: CGPoint(x: movingArrow.position.x, y: size.height + movingArrow.size.height / 2), duration: 1.0)
-		let actionMoveDone = SKAction.removeFromParent()
-		movingArrow.run(SKAction.sequence([actionMove, actionMoveDone]))
-	}
-	
 	func makeArrow(position: CGPoint) -> SKSpriteNode {
 		let arrow = SKSpriteNode(imageNamed: "arrow")
 		arrow.position = position
@@ -68,79 +50,48 @@ class GameScene: SKScene {
 		return arrow
 	}
 	
-//    private var label : SKLabelNode?
-//    private var spinnyNode : SKShapeNode?
-//
-//    override func didMove(to view: SKView) {
-//
-//        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
-//    }
-//
-//
-//    func touchDown(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//            self.addChild(n)
-//        }
-//    }
-//
-//    func touchMoved(toPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
-//    }
-//
-//    func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
-//    }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let label = self.label {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
-//
-//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-//    }
-//
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-//    }
-//
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-//    }
-//
-//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-//    }
-//
-//
-//    override func update(_ currentTime: TimeInterval) {
-//        // Called before each frame is rendered
-//    }
+	var touchedNodeHolder: SKNode?
+	var startTouchPosition: CGPoint?
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else {
+			return
+		}
+		let pointOfTouch = touch.location(in: self)
+		startTouchPosition = pointOfTouch
+		touchedNodeHolder = nodes(at: pointOfTouch).first
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else {
+			return
+		}
+		let pointOfTouch = touch.location(in: self)
+		if touchedNodeHolder == arrow {
+			touchedNodeHolder?.position = pointOfTouch
+		}
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else {
+			return
+		}
+		touchedNodeHolder = nil
+		if startTouchPosition == touch.location(in: self) {
+			shootArrow(touch: touch)
+		}
+	}
+	
+	func shootArrow(touch: UITouch) {
+		let frontTouchedNode = atPoint(touch.location(in: self))
+		if frontTouchedNode != arrow {
+			return
+		}
+
+		let movingArrow = makeArrow(position: arrow.position)
+		addChild(movingArrow)
+		let actionMove = SKAction.move(to: CGPoint(x: movingArrow.position.x, y: size.height + movingArrow.size.height / 2), duration: 1.0)
+		let actionMoveDone = SKAction.removeFromParent()
+		movingArrow.run(SKAction.sequence([actionMove, actionMoveDone]))
+	}
 }
